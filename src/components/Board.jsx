ce43c5c1a2fd114c27f79e1a749c8280c868da10
BKiptoo <bettsonk@gmail.com>
2025-07-27 14:55:15 +0300

    updated UI&board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,8 +4,16 @@ import "../styles/board.css";
 import useGoGame from "../logic/useGoGame";
 
 function Board() {
-  const { board, currentPlayer, placeStone } = useGoGame();
+  const { 
+    board, 
+    currentPlayer, 
+    capturedStones, 
+    placeStone,
+    getScore, 
+    undoMove 
+  } = useGoGame();
 
+  // Create the 9x9 grid structure
   const grid = Array(9)
     .fill()
     .map((_, row) =>
@@ -14,17 +22,27 @@ function Board() {
         .map((_, col) => ({ row, col }))
     );
 
-  const blackScore = board.flat().filter((value) => value === "black").length;
-  const whiteScore = board.flat().filter((value) => value === "white").length;
+  // Get the current score
+  const score = getScore();
 
   return (
     <div className="board">
       {/* Scoreboard */}
       <div className="scoreboard">
-        <p>Black: {blackScore}</p>
-        <p>White: {whiteScore}</p>
+        <p>Black Score: {score.black}</p>
+        <p>White Score: {score.white}</p>
         <p>Current Player: {currentPlayer}</p>
+        <p>Captured - Black: {capturedStones.black}, White: {capturedStones.white}</p>
+        
+        {/* Game control buttons */}
+        <div className="game-controls">
+          <button onClick={undoMove} className="control-button">
+            Undo Move
+          </button>
+        </div>
       </div>
+      
+      {/* The game board */}
       {grid.map((row, rowIndex) => (
         <div className="row" key={rowIndex}>
           {row.map((cell, colIndex) => (
