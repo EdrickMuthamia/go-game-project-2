b95853c6c156c383f089891d4ed896db89c563c0
Jane Nduku Kimei <Janetnduku519@gmail.com>
2025-07-29 16:03:17 +0300

    add Board component with game logic and UI

---
+++ src/components/Board.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Intersection from "./Intersection";
+import "../styles/board.css";
+import useGoGame from "../logic/useGoGame";
+
+function Board() {
+  const { board, currentPlayer, capturedStones, placeStone, getScore } =
+    useGoGame();
+
+  // Create the 9x9 grid structure
+  const grid = Array(9)
+    .fill()
+    .map((_, row) =>
+      Array(9)
+        .fill()
+        .map((_, col) => ({ row, col }))
+    );
+
+  // Get the current score
+  const score = getScore();
+
+  return (
+    <div className="board">
+      {/* Scoreboard */}
+      <div className="scoreboard">
+        <p>Black Score: {score.black}</p>
+        <p>White Score: {score.white}</p>
+        <p>Current Player: {currentPlayer}</p>
+        <p>
+          Captured - Black: {capturedStones.black}, White:{" "}
+          {capturedStones.white}
+        </p>
+      </div>
+
+      {/* The game board */}
+      {grid.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((cell, colIndex) => (
+            <Intersection
+              key={`${rowIndex}-${colIndex}`}
+              row={rowIndex}
+              col={colIndex}
+              onClick={() => placeStone(rowIndex, colIndex)}
+              value={board[rowIndex][colIndex]}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Board;
